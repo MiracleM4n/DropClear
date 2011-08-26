424298ad0b11e51fd832d620b049d7c06dc16cfc
Scott Chi <scott_m_chi@hotmail.com>
2011-08-26 14:41:59 -0400

    Moved to IntelliJ. Added support for more Permissions (As well as SuperPerms)

---
+++ src/net/D3GN/MiracleM4n/DropClear/DCConfigListener.java
@@ -0,0 +1,77 @@
+package net.D3GN.MiracleM4n.DropClear;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.util.config.Configuration;
+
+import java.io.File;
+
+public class DCConfigListener {
+    DropClear plugin;
+
+    public DCConfigListener(DropClear plugin) {
+        this.plugin = plugin;
+    }
+
+    Boolean hasChanged = false;
+
+    public void checkConfig() {
+		PluginDescriptionFile pdfFile = plugin.getDescription();
+		Configuration config = plugin.config;
+		config.load();
+
+		if (config.getProperty("NotNumb") == null) {
+			config.setProperty("NotNumb", plugin.notNumber);
+			hasChanged = true;
+		}
+		if (config.getProperty("NegValue") == null) {
+			config.setProperty("NegValue", plugin.negativeInterger);
+			hasChanged = true;
+		}
+		if (config.getProperty("TooFar") == null) {
+			config.setProperty("TooFar", plugin.farAway);
+			hasChanged = true;
+		}
+		if (config.getProperty("CantFind") == null) {
+			config.setProperty("CantFind", plugin.cantFind);
+			hasChanged = true;
+		}
+		if (config.getProperty("NoPerms") == null) {
+			config.setProperty("NoPerms", plugin.noPermissions);
+			hasChanged = true;
+		}
+		if (config.getProperty("ItemKill") == null) {
+			config.setProperty("ItemKill", plugin.itemKill);
+			hasChanged = true;
+		}
+		if (config.getProperty("Max_Kill_Radius") == null) {
+			config.setProperty("Max_Kill_Radius", plugin.maxKillRadius);
+			hasChanged = true;
+		}
+		if (config.getProperty("Max_Admin_Kill_Radius") == null) {
+			config.setProperty("Max_Admin_Kill_Radius", plugin.maxAdminKillRadius);
+			hasChanged = true;
+		}
+		if (hasChanged) {
+			config.setHeader("#DropClear  Configuration File, Enjoy!!");
+			plugin.console.sendMessage("[" + pdfFile.getName() + "]" + " config.yml has been updated.");
+			config.save();
+		}
+	}
+
+	public void readConfig() {
+		Configuration config = new Configuration(new File(plugin.getDataFolder(), "config.yml"));
+		config.load();
+
+		//Strings
+		plugin.notNumber  = config.getString("NotNumb", plugin.notNumber);
+		plugin.negativeInterger  = config.getString("NegValue", plugin.negativeInterger);
+		plugin.farAway  = config.getString("TooFar", plugin.farAway);
+		plugin.cantFind  = config.getString("CantFind", plugin.cantFind);
+		plugin.noPermissions  = config.getString("NoPerms", plugin.noPermissions);
+		plugin.itemKill  = config.getString("ItemKill", plugin.itemKill);
+
+		//Intergers
+		plugin.maxKillRadius = config.getInt("Max_Kill_Radius", plugin.maxKillRadius);
+		plugin.maxAdminKillRadius = config.getInt("Max_Admin_Kill_Radius", plugin.maxAdminKillRadius);
+	}
+}
--- src/net/D3GN/MiracleM4n/DropClear/DropClear.java
+++ src/net/D3GN/MiracleM4n/DropClear/DropClear.java
@@ -1,164 +1,71 @@
 package net.D3GN.MiracleM4n.DropClear;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.logging.Logger;
 
+import org.anjocaido.groupmanager.permissions.AnjoPermissionsHandler;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.command.ColouredConsoleSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.util.config.Configuration;
 
 public class DropClear extends JavaPlugin {
 
-	protected final static Logger logger = Logger.getLogger("Minecraft");
-	public ColouredConsoleSender console = null;
-	DCCommandExecutor commandexecutor = new DCCommandExecutor(this);
-	
+	DCCommandExecutor cExecutor = null;
+    DCConfigListener cListener = null;
+
+	ColouredConsoleSender console = null;
+    Configuration config = null;
 	
 	//Strings
-	public String notNumber = "That is not a number";
-	public String negativeInterger = "Cant Use Negative Values";
-	public String farAway = "Too far away";
-	public String cantFind = "No Items Found";
-	public String noPermissions = "You don't have permissions to use this";
-	public String itemKill = "Items Cleared";
-	public String pInfo = "[DropClear] ";
+	 String notNumber = "That is not a number";
+	 String negativeInterger = "Cant Use Negative Values";
+	 String farAway = "Too far away";
+	 String cantFind = "No Items Found";
+	 String noPermissions = "You don't have permissions to use this";
+	 String itemKill = "Items Cleared";
+	 String pInfo = "[DropClear] ";
 	
 	//Integers
-	public Integer maxKillRadius = 10;
-	public Integer maxAdminKillRadius = 30;
+	 Integer maxKillRadius = 10;
+	 Integer maxAdminKillRadius = 30;
 	
 	//Booleans
-	public Boolean messageFix = true;
-	public Boolean messFix = true;
-	public Boolean hasChanged = false;
-  	public Boolean bukkitPermission = false;
+	 Boolean messageFix = true;
+	 Boolean messFix = true;
+  	 Boolean bukkitPermission = false;
 	
-	public static PermissionHandler permissions;
+	// Permissions
+    public PermissionHandler permissions;
+    Boolean permissions3;
+    Boolean permissionsB = false;
+
+    // GroupManager
+    public AnjoPermissionsHandler gmPermissions;
+    Boolean gmPermissionsB = false;
 	
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		console = new ColouredConsoleSender((CraftServer)getServer());
-		getCommand("dropclear").setExecutor(commandexecutor);
-		getCommand("dc").setExecutor(commandexecutor);
-	        
-		setupPermissions();
+        config = new Configuration(new File(getDataFolder(), "config.yml"));
+
+        setupPermissions();
+
+	    cExecutor = new DCCommandExecutor(this);
+        cListener = new DCConfigListener(this);
+
+        cListener.checkConfig();
+		cListener.readConfig();
+
+        getCommand("dropclear").setExecutor(cExecutor);
 		
 		console.sendMessage("[" + (pdfFile.getName()) + "]" + " version " + 
 			pdfFile.getVersion() + " is enabled!");
-		
-		moveFiles();
-		checkConfig();
-		readConfig();
-	}
-	
-	public void moveFiles(){
-		getDataFolder().mkdir();
-		getDataFolder().setWritable(true);
-		getDataFolder().setExecutable(true);
-		extractFile("config.yml");
-	}
-	
-	private void extractFile(String name) {
-		PluginDescriptionFile pdfFile = getDescription();
-		File actual = new File(getDataFolder(), name);
-		if (!actual.exists()) {
-			InputStream input = getClass().getResourceAsStream("/Config/" + name);
-			if (input != null) {
-				FileOutputStream output = null;
-				try
-				{
-					output = new FileOutputStream(actual);
-					byte[] buf = new byte[8192];
-					int length = 0;
-
-					while ((length = input.read(buf)) > 0) {
-						output.write(buf, 0, length);
-					}
-					console.sendMessage("[" + (pdfFile.getName()) + "]" + " Default file written: " + name);
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						if (input != null)
-							input.close();
-					} catch (Exception e) {
-					} try {
-						if (output != null)
-							output.close();
-					} catch (Exception e) {
-					}
-				}
-			}
-		}
-	}
-	
-	public void checkConfig() {
-		PluginDescriptionFile pdfFile = getDescription();
-		Configuration config = new Configuration(new File(getDataFolder(), "config.yml"));
-		config.load();
-		
-		if (config.getProperty("NotNumb") == null) {
-			config.setProperty("NotNumb", notNumber);
-			hasChanged = true;
-		}
-		if (config.getProperty("NegValue") == null) {
-			config.setProperty("NegValue", negativeInterger);
-			hasChanged = true;
-		}
-		if (config.getProperty("TooFar") == null) {
-			config.setProperty("TooFar", farAway);
-			hasChanged = true;
-		}
-		if (config.getProperty("CantFind") == null) {
-			config.setProperty("CantFind", cantFind);
-			hasChanged = true;
-		}
-		if (config.getProperty("NoPerms") == null) {
-			config.setProperty("NoPerms", noPermissions);
-			hasChanged = true;
-		}
-		if (config.getProperty("ItemKill") == null) {
-			config.setProperty("ItemKill", itemKill);
-			hasChanged = true;
-		}
-		if (config.getProperty("Max_Kill_Radius") == null) {
-			config.setProperty("Max_Kill_Radius", maxKillRadius);
-			hasChanged = true;
-		}
-		if (config.getProperty("Max_Admin_Kill_Radius") == null) {
-			config.setProperty("Max_Admin_Kill_Radius", maxAdminKillRadius);
-			hasChanged = true;
-		}
-		if (hasChanged) {
-			config.setHeader("#DropClear  Configuration File, Enjoy!!");
-			console.sendMessage("[" + pdfFile.getName() + "]" + " config.yml has been updated.");
-			config.save();
-		}
-	}
-	
-	public void readConfig() {
-		Configuration config = new Configuration(new File(getDataFolder(), "config.yml"));
-		config.load();
-		
-		//Strings
-		notNumber  = config.getString("NotNumb", notNumber);
-		negativeInterger  = config.getString("NegValue", negativeInterger);
-		farAway  = config.getString("TooFar", farAway);
-		cantFind  = config.getString("CantFind", cantFind);
-		noPermissions  = config.getString("NoPerms", noPermissions);
-		itemKill  = config.getString("ItemKill", itemKill);
-		
-		//Intergers
-		maxKillRadius = config.getInt("Max_Kill_Radius", maxKillRadius);
-		maxAdminKillRadius = config.getInt("Max_Admin_Kill_Radius", maxAdminKillRadius);
 	}
 	    
 	public void onDisable() {
@@ -168,21 +75,53 @@ public class DropClear extends JavaPlugin {
 	}
 	   	
 	private void setupPermissions() {
-		PluginDescriptionFile pdfFile = getDescription();
-		Plugin permTest = this.getServer().getPluginManager().getPlugin("Permissions");	
-		Plugin bukkitPermTest = this.getServer().getPluginManager().getPlugin("PermissionsBukkit");
-		if (bukkitPermTest != null) {
-			bukkitPermission = true;
-			console.sendMessage("[" + (pdfFile.getName()) + "]" + " PermissionsBukkit " + (bukkitPermTest.getDescription().getVersion()) + " found hooking in.");
-		} else if (permissions == null) {
-			if (permTest != null) {
-				permissions = ((Permissions)permTest).getHandler();
-				console.sendMessage("[" + (pdfFile.getName()) + "]" + " Permissions "  + (permTest.getDescription().getVersion()) + " found hooking in.");
-			} 
-		} else {
-			bukkitPermission = true;
-			console.sendMessage("[" + (pdfFile.getName()) + "]" + " Permissions plugin not found, Defaulting to Bukkit Methods.");
-		}
-	}
+        Plugin permTest = this.getServer().getPluginManager().getPlugin("Permissions");
+        PluginDescriptionFile pdfFile = getDescription();
+
+        if(permissions != null)
+            return;
+
+        if(permTest != null) {
+            permissions = ((Permissions) permTest).getHandler();
+            permissionsB = true;
+
+            System.out.println("[" + pdfFile.getName() + "]" + " Permissions " + (permTest.getDescription().getVersion()) + " found hooking in.");
+        } else {
+            permissionsB = false;
+            System.out.println("[" + pdfFile.getName() + "]" + " Permissions not found, Checking for GroupManager.");
+            setupGroupManager();
+        }
+    }
+
+    private void setupGroupManager() {
+        Plugin permTest = this.getServer().getPluginManager().getPlugin("GroupManager");
+        PluginDescriptionFile pdfFile = getDescription();
+
+        if(gmPermissions != null)
+            return;
+
+        if (permTest != null) {
+            gmPermissionsB = true;
+            System.out.println("[" + pdfFile.getName() + "]" + " GroupManager " + (permTest.getDescription().getVersion()) + " found hooking in.");
+        } else {
+            gmPermissionsB = false;
+            System.out.println("[" + pdfFile.getName() + "]" + " GroupManager not found, Using SuperPerms.");
+        }
+    }
+
+    public Boolean checkPermissions(Player player, String node) {
+        if (permissionsB) {
+            if (permissions.has(player, node))
+                return true;
+        } else if (gmPermissionsB) {
+            if (gmPermissions.has(player, node))
+                return true;
+        } else if (player.hasPermission(node)) {
+             return true;
+        } else if (player.isOp()) {
+             return true;
+        }
+        return false;
+    }
 }
 
